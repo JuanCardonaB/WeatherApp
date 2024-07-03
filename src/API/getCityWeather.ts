9abf60ed729de6b26fea5ed7184389351ac35f7b
JuanCardonaB <juanjocarbol@gmail.com>
2024-07-02 23:10:34 -0500

    Fetch with the env

--- src/API/getCityWeather.ts
+++ src/API/getCityWeather.ts
@@ -1,6 +1,6 @@
 export const getCityWeather = async (city: string) => {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=cbc8fe2f1fc04d18aa6220654242306&q=${city}`,
+    `https://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${city}`,
   );
   const data = await response.json();
   return data;
